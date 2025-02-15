2e060910f7c9ac216ce4964db95d66170985d9bf
Oleg Shilov <shilov-1@yandex.ru>
2025-02-15 17:28:02 +0300

    updated eslint config;

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,8 +17,7 @@ module.exports = {
     "import/order": [
       "error",
       {
-        "newlines-between": "always",
-        "new-line-between-groups": "",
+        "newlines-between": "always-and-inside-groups",
         "groups": [["builtin", "external"], "internal", ["parent", "sibling", "index"]],
         "pathGroups": [
           {
