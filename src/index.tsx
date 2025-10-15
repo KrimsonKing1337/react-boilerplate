c06b221c1f248fe89d0352bd31e62755dab71c97
Oleg Shilov <shilov-1@yandex.ru>
2025-10-15 11:04:54 +0300

    updated packages;

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 
 import { store } from 'store';
 
 import { App } from './components';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLDivElement);
+const root = createRoot(document.getElementById('root') as HTMLDivElement);
 
 root.render(
   <Provider store={store}>
